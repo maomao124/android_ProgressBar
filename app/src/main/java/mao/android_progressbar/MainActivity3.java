f9b90a248f1748a4d7fb05467a3269f0557a9ec5
mao <1296193245@qq.com>
2022-10-08 12:47:21 +0800

    android_ProgressBar

--- app/src/main/java/mao/android_progressbar/MainActivity3.java
+++ app/src/main/java/mao/android_progressbar/MainActivity3.java
@@ -2,7 +2,10 @@ package mao.android_progressbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity3 extends AppCompatActivity
 {
@@ -12,5 +15,20 @@ public class MainActivity3 extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        ProgressBar progressBar = findViewById(R.id.ProgressBar);
+
+        ImageView imageView = findViewById(R.drawable.amin_pgbar);
+        AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getDrawable();
+
+
+        progressBar.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                animationDrawable.start();
+            }
+        }, 100);
     }
 }
